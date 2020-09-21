c43f3739786526242c749ee6f0d4df576047d57c
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-09-21 11:18:12 +0300

    docstring added

--- cogs/utils/securityChecks.py
+++ cogs/utils/securityChecks.py
@@ -3,6 +3,9 @@ from stellar_sdk.exceptions import Ed25519PublicKeyInvalidError
 
 
 def check_stellar_address(address):
+    """
+    Filter for withdrawals
+    """
     try:
         Account(account_id=address, sequence=0)
         return True
