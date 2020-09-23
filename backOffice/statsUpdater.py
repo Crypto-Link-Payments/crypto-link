7b2d4c1fcf45901217a3ae550623f255c932fb6b
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-09-23 12:59:11 +0300

    Code smells

--- backOffice/statsUpdater.py
+++ backOffice/statsUpdater.py
@@ -14,33 +14,39 @@ sys.path.append(project_path)
 
 helper = Helpers()
 d = helper.read_json_file(file_name='botSetup.json')
+CONST_CURRENT_DATE = '$currentDate'
+CONST_INC = '$inc'
 
 
 class StatsManager(object):
+    """
+    Class handling Crypto Link statistics
+    """
+
     def __init__(self):
         # main db connection
         self.connection = MongoClient(d['database']['connection'])
 
         # Database of bot users
-        self.clConnection = self.connection['CryptoLink']
-        self.userProfiles = self.clConnection.user_profiles
-        self.chainActivities = self.clConnection.CLOnChainStats
-        self.offChainActivities = self.clConnection.CLOffChainStats
+        self.cl_connection = self.connection['CryptoLink']
+        self.user_profiles = self.cl_connection.user_profiles
+        self.chain_activities = self.cl_connection.CLOnChainStats
+        self.off_chain_activities = self.cl_connection.CLOffChainStats
 
     def update_user_deposit_stats(self, user_id: int, amount: float, key: str):
         """
         Updates users deposit stats
         """
 
-        self.userProfiles.find_one_and_update({"userId": user_id},
-                                              {"$inc": {f"{key}.depositsCount": 1,
-                                                        f"{key}.totalDeposited": amount}})
+        self.user_profiles.find_one_and_update({"userId": user_id},
+                                               {"$inc": {f"{key}.depositsCount": 1,
+                                                         f"{key}.totalDeposited": amount}})
 
     def update_user_withdrawal_stats(self, user_id, amount, key: str):
         """Updates users withdrawal stats"""
-        self.userProfiles.find_one_and_update({"userId": user_id},
-                                              {"$inc": {f"{key}.withdrawalsCount": 1,
-                                                        f"{key}.totalWithdrawn": amount}})
+        self.user_profiles.find_one_and_update({"userId": user_id},
+                                               {f"{CONST_INC}": {f"{key}.withdrawalsCount": 1,
+                                                                 f"{key}.totalWithdrawn": amount}})
 
     def update_bot_chain_stats(self, type_of: str, ticker: str, amount: float):
         """
@@ -48,15 +54,15 @@ class StatsManager(object):
         """
 
         if type_of == "deposit":
-            self.chainActivities.update_one({"ticker": f"{ticker}"},
-                                            {"$inc": {"depositCount": 1,
-                                                      "depositAmount": round(float(amount), 7)},
-                                             "$currentDate": {"lastModified": True}})
+            self.chain_activities.update_one({"ticker": f"{ticker}"},
+                                             {f"{CONST_INC}": {"depositCount": 1,
+                                                               "depositAmount": round(float(amount), 7)},
+                                              f"{CONST_CURRENT_DATE}": {"lastModified": True}})
         elif type_of == "withdrawal":
-            self.chainActivities.update_one({"ticker": f"{ticker}"},
-                                            {"$inc": {"withdrawalCount": 1,
-                                                      "withdrawnAmount": round(float(amount), 7)},
-                                             "$currentDate": {"lastModified": True}})
+            self.chain_activities.update_one({"ticker": f"{ticker}"},
+                                             {f"{CONST_INC}": {"withdrawalCount": 1,
+                                                               "withdrawnAmount": round(float(amount), 7)},
+                                              f"{CONST_CURRENT_DATE}": {"lastModified": True}})
 
     def update_user_transaction_stats(self, user_id: int, key, amount: float, direction: str, tx_type: str,
                                       special: str = None, mined: float = None):
@@ -101,47 +107,47 @@ class StatsManager(object):
             if direction == 'outgoing':
                 data[f"clCoinStats.mined"] = mined
 
-        self.userProfiles.find_one_and_update({"userId": user_id}, {"$inc": data})
+        self.user_profiles.find_one_and_update({"userId": user_id}, {f"{CONST_INC}": data})
 
     def update_bot_off_chain_stats(self, ticker: str, tx_amount: int, xlm_amount: float, tx_type: str):
         """
         update bot stats based on transaction type in the system
         """
         if tx_type == 'public':
-            self.offChainActivities.update_one({"ticker": f"{ticker}"},
-                                               {"$inc": {"totalTx": int(tx_amount),
-                                                         "totalMoved": xlm_amount,
-                                                         "totalPublicCount": 1,
-                                                         "totalPublicMoved": xlm_amount},
-                                                "$currentDate": {"lastModified": True}})
+            self.off_chain_activities.update_one({"ticker": f"{ticker}"},
+                                                 {f"{CONST_INC}": {"totalTx": int(tx_amount),
+                                                                   "totalMoved": xlm_amount,
+                                                                   "totalPublicCount": 1,
+                                                                   "totalPublicMoved": xlm_amount},
+                                                  f"{CONST_CURRENT_DATE}": {"lastModified": True}})
 
         elif tx_type == 'private':
-            self.offChainActivities.update_one({"ticker": f"{ticker}"},
-                                               {"$inc": {"totalTx": int(tx_amount),
-                                                         "totalMoved": xlm_amount,
-                                                         "totalPrivateCount": 1,
-                                                         "totalPrivateMoved": xlm_amount},
-                                                "$currentDate": {"lastModified": True}})
+            self.off_chain_activities.update_one({"ticker": f"{ticker}"},
+                                                 {f"{CONST_INC}": {"totalTx": int(tx_amount),
+                                                                   "totalMoved": xlm_amount,
+                                                                   "totalPrivateCount": 1,
+                                                                   "totalPrivateMoved": xlm_amount},
+                                                  f"{CONST_CURRENT_DATE}": {"lastModified": True}})
 
         elif tx_type == 'emoji':
-            self.offChainActivities.update_one({"ticker": f"{ticker}"},
-                                               {"$inc": {"totalTx": int(tx_amount),
-                                                         "totalMoved": xlm_amount,
-                                                         "totalEmojiTx": 1,
-                                                         "totalEmojiMoved": xlm_amount},
-                                                "$currentDate": {"lastModified": True}})
+            self.off_chain_activities.update_one({"ticker": f"{ticker}"},
+                                                 {f"{CONST_INC}": {"totalTx": int(tx_amount),
+                                                                   "totalMoved": xlm_amount,
+                                                                   "totalEmojiTx": 1,
+                                                                   "totalEmojiMoved": xlm_amount},
+                                                  f"{CONST_CURRENT_DATE}": {"lastModified": True}})
         elif tx_type == 'multiTx':
-            self.offChainActivities.update_one({"ticker": f"{ticker}"},
-                                               {"$inc": {"totalTx": int(tx_amount),
-                                                         "totalMoved": xlm_amount,
-                                                         "multiTxCount": 1,
-                                                         "multiTxMoved": xlm_amount},
-                                                "$currentDate": {"lastModified": True}})
+            self.off_chain_activities.update_one({"ticker": f"{ticker}"},
+                                                 {f"{CONST_INC}": {"totalTx": int(tx_amount),
+                                                                   "totalMoved": xlm_amount,
+                                                                   "multiTxCount": 1,
+                                                                   "multiTxMoved": xlm_amount},
+                                                  f"{CONST_CURRENT_DATE}": {"lastModified": True}})
 
         elif tx_type == 'rolePurchase':
-            self.offChainActivities.update_one({"ticker": f"{ticker}"},
-                                               {"$inc": {"totalTx": int(tx_amount),
-                                                         "totalMoved": xlm_amount,
-                                                         "rolePurchaseTxCount": 1,
-                                                         "roleMoved": xlm_amount},
-                                                "$currentDate": {"lastModified": True}})
+            self.off_chain_activities.update_one({"ticker": f"{ticker}"},
+                                                 {f"{CONST_INC}": {"totalTx": int(tx_amount),
+                                                                   "totalMoved": xlm_amount,
+                                                                   "rolePurchaseTxCount": 1,
+                                                                   "roleMoved": xlm_amount},
+                                                  f"{CONST_CURRENT_DATE}": {"lastModified": True}})
