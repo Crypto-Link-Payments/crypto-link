3abc6cb3e8cc80623836532c15156d4834940ab3
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-09-25 08:46:15 +0300

    Fixed Merchant Stats Updates

--- backOffice/statsUpdater.py
+++ backOffice/statsUpdater.py
@@ -5,7 +5,7 @@ Script to handle statistics of the bot
 import os
 import sys
 
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 from utils.tools import Helpers
 
@@ -29,7 +29,7 @@ class StatsManager(object):
 
         # Database of bot users
         self.cl_connection = self.connection['CryptoLink']
-        self.user_profiles = self.cl_connection.user_profiles
+        self.user_profiles = self.cl_connection.userProfiles
         self.chain_activities = self.cl_connection.CLOnChainStats
         self.off_chain_activities = self.cl_connection.CLOffChainStats
 
@@ -100,15 +100,20 @@ class StatsManager(object):
                 data["transactionCounter.emojiTxCount"] = 1
             elif special == 'multiTx':
                 data["transactionCounter.multiTxCount"] = 1
-            elif special == 'rolePurchase':
-                data["transactionCounter.rolePurchase"] = 1
-                data[f"{key}.spentOnRoles"] = amount
 
         # Mined or not
         if mined and direction == 'outgoing':
             data[f"clCoinStats.mined"] = mined
 
         self.user_profiles.find_one_and_update({"userId": user_id}, {f"{CONST_INC}": data})
+
+    def update_user_role_purchase_stats(self, user_id: int, key_to_update: str, amount: float):
+
+        to_update = dict()
+        to_update["transactionCounter.rolePurchase"] = 1
+        to_update[f"{key_to_update}.spentOnRoles"] = amount
+        self.user_profiles.update_one({"userId": user_id},
+                                      {f"$inc": to_update})
 
     def update_bot_off_chain_stats(self, ticker: str, tx_amount: int, xlm_amount: float, tx_type: str):
         """
@@ -152,3 +157,6 @@ class StatsManager(object):
                                                                    "rolePurchaseTxCount": 1,
                                                                    "roleMoved": xlm_amount},
                                                   f"{CONST_CURRENT_DATE}": {"lastModified": True}})
+
+
+StatsManager().update_user_role_purchase_stats(user_id=360367188432912385, key_to_update='xlmStats', amount=100.0)
