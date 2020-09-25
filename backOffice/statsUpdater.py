421fd4972668a0f33d5ac037246804efbe713c9b
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-09-25 09:11:24 +0300

    Async support merchant user update stats

--- backOffice/statsUpdater.py
+++ backOffice/statsUpdater.py
@@ -5,7 +5,8 @@ Script to handle statistics of the bot
 import os
 import sys
 
-from pymongo import MongoClient, errors
+from pymongo import MongoClient
+import motor.motor_asyncio
 
 from utils.tools import Helpers
 
@@ -26,12 +27,17 @@ class StatsManager(object):
     def __init__(self):
         # main db connection
         self.connection = MongoClient(d['database']['connection'])
+        self.as_connection = motor.motor_asyncio.AsyncIOMotorClient(d['database']['connection'])  # Async connection
 
         # Database of bot users
         self.cl_connection = self.connection['CryptoLink']
         self.user_profiles = self.cl_connection.userProfiles
         self.chain_activities = self.cl_connection.CLOnChainStats
         self.off_chain_activities = self.cl_connection.CLOffChainStats
+
+        # Async support
+        self.as_cl_connection = self.as_connection['CryptoLink']
+        self.as_user_profiles = self.as_cl_connection.userProfiles
 
     def update_user_deposit_stats(self, user_id: int, amount: float, key: str):
         """
@@ -107,13 +113,12 @@ class StatsManager(object):
 
         self.user_profiles.find_one_and_update({"userId": user_id}, {f"{CONST_INC}": data})
 
-    def update_user_role_purchase_stats(self, user_id: int, key_to_update: str, amount: float):
-
+    async def as_update_role_purchase_stats(self, user_id: int, key_to_update: str, amount: float):
         to_update = dict()
         to_update["transactionCounter.rolePurchase"] = 1
         to_update[f"{key_to_update}.spentOnRoles"] = amount
-        self.user_profiles.update_one({"userId": user_id},
-                                      {f"$inc": to_update})
+        await self.as_user_profiles.update_one({"userId": user_id},
+                                               {f"$inc": to_update})
 
     def update_bot_off_chain_stats(self, ticker: str, tx_amount: int, xlm_amount: float, tx_type: str):
         """
@@ -157,6 +162,3 @@ class StatsManager(object):
                                                                    "rolePurchaseTxCount": 1,
                                                                    "roleMoved": xlm_amount},
                                                   f"{CONST_CURRENT_DATE}": {"lastModified": True}})
-
-
-StatsManager().update_user_role_purchase_stats(user_id=360367188432912385, key_to_update='xlmStats', amount=100.0)
