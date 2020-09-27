2ffd2cb85d9c87fe41b0f57020b03ebb985da3dc
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-09-27 20:20:12 +0300

    added __init__.py removed print statement

--- backOffice/stellarOnChainHandler.py
+++ backOffice/stellarOnChainHandler.py
@@ -15,7 +15,6 @@ sys.path.append(project_path)
 
 helpers = Helpers()
 secret_details = helpers.read_json_file(file_name="walletSecrets.json")  # Load Stellar wallet secrets
-print(secret_details)
 public_details = helpers.read_json_file(file_name="hotWallets.json")  # Load hot wallet details
 
 
@@ -204,3 +203,6 @@ class StellarWallet:
 
             else:
                 return {}
+
+
+
