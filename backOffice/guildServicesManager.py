d288ac767b2015dc4f3b6d4ddd145e4e8b24d7ff
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-09-27 14:58:46 +0300

    async support added as well as db management functions

--- backOffice/guildServicesManager.py
+++ backOffice/guildServicesManager.py
@@ -5,7 +5,6 @@ import os
 import sys
 
 from pymongo import MongoClient
-from pymongo import errors
 import motor.motor_asyncio
 
 from utils.tools import Helpers
@@ -22,33 +21,38 @@ class GuildProfileManager:
 
     def __init__(self):
         self.connection = MongoClient(d['database']['connection'], maxPoolSize=20)
-        self.guild_activity = self.connection['CryptoLink']
-        self.guild_profiles = self.guild_activity.guildProfiles
+        self.cl_db_access = self.connection['CryptoLink']
+        self.guild_profiles = self.cl_db_access.guildProfiles
 
         self.as_connection = motor.motor_asyncio.AsyncIOMotorClient(d['database']['connection'])
-        self.as_guild_profiles = self.as_connection.guildProfiles  # Connection to user profiles
+        self.as_cl_db_access = self.as_connection['CryptoLink']
+        self.as_guild_profiles = self.as_cl_db_access.guildProfiles  # Connection to user profiles
 
     def check_guild_registration_stats(self, guild_id: int):
         result = list(self.guild_profiles.find({"guildId": guild_id}))
         return result
 
+    #
     async def register_guild(self, guild_data: dict):
-        result = await self.as_guild_profiles.insert_one(guild_data)
-        return result.id
+        result = self.as_guild_profiles.insert_one(guild_data)
+        return result
 
     async def get_all_explorer_applied_channels(self):
-        result = [guild for guild in await self.guild_profiles.find({}, {"explorerChannel"}) if
-                  guild["explorerChannel"] > 0]
+        await self.guild_profiles.find({}, {"_id": 0, "explorerSettings": 1})
+
+        result = [guild for guild in await self.guild_profiles.find({}, {"_id": 0, "explorerSettings": 1}) if
+                  int(guild["explorerChannel"]) > 0]
         return result
 
     async def get_guild_stats(self, guild_id: int):
-        return await self.as_guild_profiles.find_one({"guildId": guild_id},
-                                                     {"_id": 0,
-                                                      "communityStats": 1})
+        stats = await self.as_guild_profiles.find_one({"guildId": guild_id},
+                                                      {"_id": 0,
+                                                       "communityStats": 1})
+        return stats["communityStats"]
 
     async def update_guild_profile(self, guild_id, data_to_update: dict):
         result = await self.as_guild_profiles.update_one({"guildId": guild_id},
-                                                         {"$set": {data_to_update}})
+                                                         {"$set": data_to_update})
         return result.modified_count > 0
 
     async def get_service_statuses(self, guild_id: int):
