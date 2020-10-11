454f08af33ac495e15e61e704f7c465c0a76068c
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-10-11 09:44:03 +0300

    merchantCogs.py --> guildMerchant.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -10,7 +10,7 @@ init(autoreset=True)
 
 extensions = ['cogs.help', 'cogs.transactionCogs', 'cogs.userAccountCogs',
               'cogs.systemMngCogs', 'cogs.withdrawalCogs',
-              'cogs.merchantCogs', 'cogs.consumer', 'cogs.automatic', 'cogs.merchantLicensingCogs',
+              'cogs.guildMerchant', 'cogs.consumer', 'cogs.automatic', 'cogs.merchantLicensingCogs',
               'cogs.feeManagementCogs', 'cogs.guildOwners']
 
 
