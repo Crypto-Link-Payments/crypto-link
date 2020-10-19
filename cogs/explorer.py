483735f660e2b282caeaf5d580796cc04245fa00
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-10-19 15:34:45 +0300

    Commands Initi for asset queries

--- cogs/explorer.py
+++ cogs/explorer.py
@@ -5,11 +5,10 @@ from cogs.utils.customCogChecks import user_has_wallet
 custom_messages = CustomMessages()
 
 CONST_STELLAR_EMOJI = '<:stelaremoji:684676687425961994>'
-CONST_MERCHANT_ROLE_ERROR = "__Merchant System Role Error__"
-CONST_MERCHANT_PURCHASE_ERROR = ":warning: __Merchant System Purchase Error__:warning: "
+CONST_ACCOUNT_ERROR = "__Account Not Registered__"
 
 
-class StellarExpertCommands(commands.Cog):
+class AssetCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.backoffice = bot.backoffice
@@ -17,17 +16,34 @@ class StellarExpertCommands(commands.Cog):
 
     @commands.group()
     @commands.check(user_has_wallet)
-    async def expert(self):
+    async def asset(self):
+        """
+        Command Entry Point for Stellar Expert Queries
+        """
         pass
 
-    @expert.error
-    async def expert_error(self, ctx, error):
+    @asset.command()
+    async def supply(self):
+        """
+        Returns the asset_supply for asset
+        """
+        pass
+
+    @asset.command()
+    async def rating(self):
+        """
+        Returns the raiting on the asset
+        """
+        pass
+
+    @asset.error
+    async def asset_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
             message = f'In order to user Stellar Expert Commands you need to have wallet registered in the system!. Use' \
                       f' `{self.command_string}register`'
             await custom_messages.system_message(ctx=ctx, color_code=1, message=message, destination=0,
-                                                 sys_msg_title=CONST_MERCHANT_ROLE_ERROR)
+                                                 sys_msg_title=CONST_ACCOUNT_ERROR)
 
 
 def setup(bot):
-    bot.add_cog(StellarExpertCommands(bot))
+    bot.add_cog(AssetCommands(bot))
