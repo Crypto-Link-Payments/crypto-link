af2e0098f8a8b7abe2877dbd375bb88726c72736
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-10-19 16:08:04 +0300

    !horizon queries discord commands INIT and new account creation

---
+++ cogs/horizonQueries.py
@@ -0,0 +1,85 @@
+"""
+Cogs to handle commands for licensing with the bot
+
+Owners of the community can pay a one time monthly fee which allows them to make unlimited transfers
+from Merchant wallet to their won upon withdrawal.
+"""
+
+from discord.ext import commands
+from discord import Embed, Color
+from cogs.utils.customCogChecks import has_wallet
+from backOffice.stellarOnChainHandler import StellarWallet
+from cogs.utils.systemMessaages import CustomMessages
+from utils.tools import Helpers
+
+custom_messages = CustomMessages()
+helper = Helpers()
+auto_channels = helper.read_json_file(file_name='autoMessagingChannels.json')
+
+CONST_STELLAR_EMOJI = "<:stelaremoji:684676687425961994>"
+CONST_ACCOUNT_ERROR = '__Account Not Registered__'
+stellar_chain = StellarWallet()
+
+
+class HorizonExplorerCommands(commands.Cog):
+    """
+    Discord Commands dealing with Merchant Licensing
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.backoffice = bot.backoffice
+        self.command_string = bot.get_command_str()
+
+    @commands.group()
+    @commands.check(has_wallet)
+    async def horizon(self):
+        pass
+
+    @commands.group()
+    async def account(self):
+        pass
+
+    @account.command()
+    async def new(self, ctx):
+        """
+        Creates new in-active account on Stellar Network
+        """
+        details = stellar_chain.create_stellar_account()
+        if details:
+            new_account = Embed(title=f':rocket: New Stellar Account Created :rocket:',
+                                description=f'You have successfully created new account on {details["network"]} '
+                                            f'network.',
+                                )
+            new_account.add_field(name=f':map: Public Address :map: ',
+                                  value=f'```{details["address"]}```',
+                                  inline=False)
+            new_account.add_field(name=f':key: Secret :key: ',
+                                  value=f'```{details["secret"]}```')
+            new_account.add_field(name=f':warning: Important Message:warning:',
+                                  value=f'Please store/backup account details somewhere safe and delete this embed on'
+                                        f' Discord. Exposure of Secret to any other entity or 3rd party application'
+                                        f'might result in loss of funds. Crypto Link does not store details of newly'
+                                        f' generate account nor can recover it. This message will self-destruct in '
+                                        f' 360 seconds.')
+            await ctx.author.send(embed=new_account, delete_after=360)
+        else:
+            message = f'New Stellar Account could not be created at this moment. Please try again later.'
+            await custom_messages.system_message(ctx=ctx, color_code=1, message=message, destination=0,
+                                                 sys_msg_title=CONST_ACCOUNT_ERROR)
+
+    @account.command()
+    async def details(self, ctx):
+        pass
+
+    @horizon.error
+    async def asset_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            message = f'In order to user Stellar Expert Commands you need to have wallet registered in the system!. Use' \
+                      f' `{self.command_string}register`'
+            await custom_messages.system_message(ctx=ctx, color_code=1, message=message, destination=0,
+                                                 sys_msg_title=CONST_ACCOUNT_ERROR)
+
+
+def setup(bot):
+    bot.add_cog(HorizonExplorerCommands(bot))
