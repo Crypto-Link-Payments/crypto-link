b0194ad0122060ce1b374e01d64cf51edc6314a7
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-10-21 09:06:15 +0300

    :rocket: !horizon transactions

--- horizonCommands/transactions.py
+++ horizonCommands/transactions.py
@@ -50,17 +50,80 @@ class HorizonTransactions(commands.Cog):
                                                 description=description,
                                                 destination=1, c=Colour.lighter_gray())
 
+    @staticmethod
+    async def process_server_response(ctx, query: str, data, title: str):
+        if title == 'ledger':
+            emoji = ':ledger'
+        elif title == 'Transaction Hash':
+            emoji = ':hash:'
+        elif title == "account":
+            emoji = ':map:'
+
+        payment_details = Embed(title=f':mag_right: {title} Transactions Details :mag_right: ',
+                                colour=Colour.lighter_gray())
+        payment_details.add_field(name=f'{emoji} {title} {emoji}',
+                                  value=f'```{query}```',
+                                  inline=False)
+        payment_details.add_field(name=f'Full list of request',
+                                  value=f'{data["_links"]["self"]}')
+        await ctx.author.send(embed=payment_details)
+
+        records = data["_embedded"]["records"]
+
+        for r in records:
+            signers_data = ', '.join(
+                [f'`{sig}`' for sig in
+                 records["signatures"]])
+
+            tx_info = Embed(title=f'Transaction Details',
+                            colour=Colour.lighter_gray())
+            tx_info.set_author(name=f':id: `{r["id"]}`')
+            tx_info.add_field(name=f'Height',
+                              value=f'`{r["paging_token"]}`')
+            tx_info.add_field(name=f':ledger:ledger :ledger:',
+                              value=f'`{r["ledger"]}`')
+            tx_info.add_field(name=f':hash: Transaction Hash :hash:',
+                              value=f'`{r["hash"]}`')
+            tx_info.add_field(name=f'Memo Type',
+                              value=f'`{r["memo_type"]}`')
+            tx_info.add_field(name=f':calendar: Date and time :calendar: ',
+                              value=f'`{r["created_at"]}`')
+            tx_info.add_field(name=f'Paging Token',
+                              value=f'{r["paging_token"]}',
+                              inline=False)
+            tx_info.add_field(name='Source account',
+                              value=f'```{r["source_account"]}```',
+                              inline=False)
+            tx_info.add_field(name='Source account Sequence',
+                              value=f'```{r["source_account_sequence"]}```',
+                              inline=False)
+            tx_info.add_field(name='Fee account',
+                              value=f'```{r["fee_account"]}```',
+                              inline=False)
+            tx_info.add_field(name='Sum of operations',
+                              value=f'```{r["operation_count"]}```',
+                              inline=False)
+            tx_info.add_field(name=f'Fees',
+                              value=f'Charged: {r["fee_charged"]}\n'
+                                    f'Max: {r["max_fee"]} ')
+            tx_info.add_field(name=f'Signers',
+                              value=f'')
+            await ctx.author.send(embed=tx_info)
+
     @transactions.command()
     async def single(self, ctx, transaction_hash: str):
         data = stellar_chain.get_transactions_hash(tx_hash=transaction_hash)
+        await self.process_server_response(ctx=ctx, query=str(transaction_hash), data=data, title='Transaction Hash')
 
     @transactions.command()
     async def account(self, ctx, account_address: str):
         data = stellar_chain.get_transactions_account(address=account_address)
+        await self.process_server_response(ctx=ctx, query=str(account_address), data=data, title='account')
 
     @transactions.command()
     async def ledger(self, ctx, ledger_id: int):
         data = stellar_chain.get_transactions_ledger(ledger_id=ledger_id)
+        await self.process_server_response(ctx=ctx, query=str(ledger_id), data=data, title='Ledger')
 
 
 def setup(bot):
