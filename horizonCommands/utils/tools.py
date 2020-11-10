a714ad496798bc72478a108d1445a37737a0b278
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-11-10 13:43:48 +0200

    Branched out general functions for horizon part

--- horizonCommands/utils/tools.py
+++ horizonCommands/utils/tools.py
@@ -4,3 +4,11 @@ from datetime import datetime
 def format_date(date: str):
     dt_format = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     return dt_format
+
+def get_emoji(self, title):
+    if title == 'ledger':
+        return ':ledger:'
+    elif title == 'Transaction Hash':
+        return ':hash:'
+    elif title == "account":
+        return ':map:'
