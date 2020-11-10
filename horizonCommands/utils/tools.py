4290b3620da163b47605d2a42f3044ac7ca43b67
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-11-10 13:51:49 +0200

    :bug: on get_emoji funciton

--- horizonCommands/utils/tools.py
+++ horizonCommands/utils/tools.py
@@ -5,7 +5,8 @@ def format_date(date: str):
     dt_format = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     return dt_format
 
-def get_emoji(self, title):
+
+def get_emoji(title):
     if title == 'ledger':
         return ':ledger:'
     elif title == 'Transaction Hash':
