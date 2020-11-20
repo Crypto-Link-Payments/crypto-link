78da7bbc6359ff9645ff5c47de79f994c42c1a45
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-11-20 17:53:28 +0200

    3 level account db management

---
+++ backOffice/thirdLevelWalletManager.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_path)
+
+
+class ThirdLevelWalletManager:
+    """Class dealing with the level three walets"""
+
+    def __init__(self, connection):
+        """Connection to Database and Crypto Link collections"""
+        self.connection = connection
+        self.bot_stuff = self.connection['CryptoLink']
+        self.third_level = self.bot_stuff.ThirdLevelWallets
+
+    def third_level_user_reg_status(self, user_id: int):
+        """
+        Check user registration status
+        """
+        data = self.third_level.find_one({"userId": int(user_id)})
+        if data:
+            return True
+        else:
+            return False
+
+    def create_user_wallet(self, data_to_store: dict):
+        """
+        Creates the user hot wallet into the database. Decryption happens in COGS
+        """
+        result = self.third_level.insert_one(data_to_store)
+
+        if result.inserted_id:
+            return True
+        else:
+            return False
+
+    def get_third_account_details(self, user_id: int):
+        """
+        Get user hot wallet details for decription in cogs
+        """
+        data = self.third_level.find_one({"userId": int(user_id)},
+                                         {"_id": 0})
+        if data:
+            return data
+        else:
+            return {}
+
+    def get_third_hot_wallet_addr(self, user_id: int):
+        """
+        Get user hot wallet details for decription in cogs
+        """
+        data = self.hotWallets.find_one({"userId": int(user_id)},
+                                        {"_id": 0,
+                                         "publicAddress": 1})
+        if data:
+            return data["publicAddress"]
+        else:
+            return {}
