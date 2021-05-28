9994589fda6e1e77613cdbbeed63e829b50b56c5
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-05-28 16:58:31 +0300

    :hammer: membership key words

--- cogs/consumer.py
+++ cogs/consumer.py
@@ -24,7 +24,7 @@ class ConsumerCommands(commands.Cog):
         self.backoffice = bot.backoffice
         self.command_string = bot.get_command_str()
 
-    @commands.group(alieases=["Subscription"])
+    @commands.group(alieases=["subscription","perk","perks"])
     @commands.check(guild_has_merchant)
     @commands.check(has_wallet)
     @commands.check(is_public)
