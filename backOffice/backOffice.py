901f98ff1032e37a7cb8199e6c805869a84fb491
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-07-13 07:04:39 +0300

    :hammer: added xlm profile check and entry on start

--- backOffice/backOffice.py
+++ backOffice/backOffice.py
@@ -46,3 +46,4 @@ class BackOffice:
         self.backend_check.check_collections()
         self.backend_check.checking_stats_documents()
         self.backend_check.checking_bot_wallets()
+        self.backend_check.check_xlm_integration()
