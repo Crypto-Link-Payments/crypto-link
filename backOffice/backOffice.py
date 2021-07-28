7c68b970c009d8b5438bf3671832bef13db52196
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-07-28 08:50:20 +0300

    :recycle: Backoffice class does not need token list

--- backOffice/backOffice.py
+++ backOffice/backOffice.py
@@ -21,7 +21,6 @@ from backOffice.tokenManager import TokenManager
 class BackOffice:
     def __init__(self, bot_settings: dict):
         self.helper = Helpers()
-        self.integrated_coins = self.helper.read_json_file(file_name='integratedCoins.json')
         self.auto_messaging_channels = self.helper.read_json_file(file_name='autoMessagingChannels.json')
         self.connection = MongoClient(bot_settings['database']['connection'], maxPoolSize=20)
         self.as_connection = motor.motor_asyncio.AsyncIOMotorClient(bot_settings['database']['connection'])
@@ -30,8 +29,7 @@ class BackOffice:
         self.backend_check = BotStructureCheck(self.connection)
         self.second_level_manager = SecondLevelWalletManager(self.connection)
         self.third_level_manager = ThirdLevelWalletManager(self.connection)
-        self.stellar_wallet = StellarWallet(network_type=bot_settings["mainNet"],
-                                            integrated_coins=self.integrated_coins)
+        self.stellar_wallet = StellarWallet(network_type=bot_settings["mainNet"])
         self.merchant_manager = MerchantManager(self.connection)
         self.stellar_manager = StellarManager(self.connection, self.as_connection)
         self.stats_manager = StatsManager(self.connection, self.as_connection)
