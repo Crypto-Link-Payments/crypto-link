b2bf4074ed7dc33411a16fb392485514eaa67483
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-07-29 20:30:42 +0300

    :hammer: queries and modifications for prefixed from database

--- backOffice/guildServicesManager.py
+++ backOffice/guildServicesManager.py
@@ -15,6 +15,7 @@ class GuildProfileManager:
         self.connection = connection
         self.cl_db_access = self.connection['CryptoLink']
         self.guild_profiles = self.cl_db_access.guildProfiles
+        self.guild_prefixes = self.cl_db_access.guildPrefixes
 
         self.as_connection = as_connection
         self.as_cl_db_access = self.as_connection['CryptoLink']
@@ -42,7 +43,7 @@ class GuildProfileManager:
 
     async def update_stellar_community_wallet_stats(self, guild_id: int, data: dict):
         result = await self.as_stellar_community_wallets.update_one({"communityId": guild_id},
-                                                              {"$inc": data})
+                                                                    {"$inc": data})
 
         return result.matched_count > 0
 
@@ -59,9 +60,24 @@ class GuildProfileManager:
 
     def get_all_guild_ids(self):
         return list(self.guild_profiles.find({},
-                                             {"_id":0,
-                                              "guildId":1}))
+                                             {"_id": 0,
+                                              "guildId": 1}))
 
-    def custom_guild_prefix(self):
-        pass
+    def check_guild_prefix(self, guild_id: int):
+        return self.guild_prefixes.find_one({"guildId": guild_id},
+                                            {"_id": 0, "prefix": 1})
 
+    def set_guild_prefix(self, guild_id: int, prefix: str):
+        return self.guild_prefixes.insert_one({"guildId": guild_id,
+                                               "prefix": prefix})
+
+    def update_guild_prefix(self, guild_id, prefix: str):
+        result = self.guild_prefixes.update_one({"guildId": guild_id},
+                                                {"$set": {"prefix": prefix}})
+        return result.matched_count > 0
+
+    def get_guild_prefix(self, guild_id):
+        prefix = self.guild_prefixes.find_one({"guildId": guild_id},
+                                              {"_id": 0,
+                                               "prefix": 1})
+        return prefix["prefix"]
